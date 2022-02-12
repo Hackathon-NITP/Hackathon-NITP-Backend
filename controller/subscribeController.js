a3a4a94bd2d351642608ece2fae0ba166fa1a74c
Ujjwal Garg <ujjwalgarg.252@gmail.com>
2022-02-13 04:17:14 +0530

    changes

--- controller/subscribeController.js
+++ controller/subscribeController.js
@@ -1,32 +1,32 @@
 const {
-	sendWelcomeEmail,
-	sendNotificationEmailIfCampExists
-} = require('../utils/email');
-const Subscriber = require('../models/subscribeModel');
-const Camp = require('../models/campModel');
+  sendWelcomeEmail,
+  sendNotificationEmailIfCampExists,
+} = require("../utils/email");
+const Subscriber = require("../models/subscribeModel");
+const Camp = require("../models/campModel");
 
 const Subscribe = async (req, res) => {
-	try {
-		const subscriber = new Subscriber(req.body);
-		await subscriber.save();
+  try {
+    const subscriber = new Subscriber(req.body);
+    await subscriber.save();
 
-		res.status(200).send(subscriber);
-		const pincode = subscriber.pincode;
+    const pincode = subscriber.pincode;
 
-		sendWelcomeEmail(subscriber.name, subscriber.email);
+    sendWelcomeEmail(subscriber.name, subscriber.email);
 
-		Camp.findByCredentialsOfPincode(pincode).then(camps => {
-			if (!camps) {
-				return;
-			}
-			sendNotificationEmailIfCampExists(subscriber.name, subscriber.email);
-			// console.log('Notification Mail Sent');
-		});
-	} catch (error) {
-		res.status(400).send(e.message);
-	}
+    Camp.findByCredentialsOfPincode(pincode).then((camps) => {
+      if (!camps) {
+        return;
+      }
+      sendNotificationEmailIfCampExists(subscriber.name, subscriber.email);
+      // console.log('Notification Mail Sent');
+    });
+    res.status(200).send(subscriber);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 module.exports = {
-	Subscribe
+  Subscribe,
 };
